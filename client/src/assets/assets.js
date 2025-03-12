221762738ddf750d10f5d1eb7e5487acf15143e3
Easir Arafat <riyadhbd2@Easirs-MacBook-Air.local>
2025-03-12 02:01:36 +0100

    feature responsive added

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -12,6 +12,14 @@ import f5 from '../assets/f-5.png';
 import f6 from '../assets/f-6.png';
 import f7 from '../assets/f-7.png';
 import f8 from '../assets/f-8.png';
+import f9 from '../assets/f-9.png';
+import f10 from '../assets/f-10.png';
+import f11 from '../assets/f-11.png';
+import f12 from '../assets/f-12.png';
+import f13 from '../assets/f-13.png';
+import f14 from '../assets/f-14.png';
+import f15 from '../assets/f-15.png';
+import f16 from '../assets/f-16.png';
 
 
 
@@ -23,4 +31,4 @@ export const assets = {
 
 export const images = [one, two, three];
 
-export const features = [ f1, f2, f3, f4, f5, f6, f7, f8 ];
+export const features = [ f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15, f16 ];
--- client/src/components/Features.jsx
+++ client/src/components/Features.jsx
@@ -4,24 +4,21 @@ import { features } from "../assets/assets";
 const Features = () => {
   return (
     <div className="mt-5">
-      <div className="px-40">
+      <div className="px-4 sm:px-10 md:px-20 lg:px-40">
         <div className="bg-gray-200 w-full h-5"></div>
         <div className="m-2">
           <h1 className="text-center text-3xl">Featured Categories</h1>
-          <div className="grid grid-cols-4">
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 mt-5 gap-2">
             {features.map((feature, index) => (
               <div
                 key={index}
-                className="flex justify-center items-center cursor-pointer mt-5 border"
+                className="flex justify-center items-center cursor-pointer"
               >
-                <div className="border">
-                  <img src={feature} alt="feature" className="" />
-                </div>
+                <img src={feature} alt="feature" className="max-w-full h-auto" />
               </div>
             ))}
           </div>
         </div>
-
         <div className="bg-gray-200 w-full h-5"></div>
       </div>
     </div>
