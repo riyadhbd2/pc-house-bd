59199edebd5d765a8c58a7a627b89cc1178742ed
Easir Arafat <riyadhbd2@Easirs-MacBook-Air.local>
2025-03-12 02:06:29 +0100

    feature category corrected

--- client/src/components/Features.jsx
+++ client/src/components/Features.jsx
@@ -6,20 +6,24 @@ const Features = () => {
     <div className="mt-5">
       <div className="px-4 sm:px-10 md:px-20 lg:px-40">
         <div className="bg-gray-200 w-full h-5"></div>
-        <div className="m-2">
-          <h1 className="text-center text-3xl">Featured Categories</h1>
+        <div className="">
+          <h1 className="text-center text-3xl py-2">Featured Categories</h1>
+          <div className="bg-gray-200 w-full h-5"></div>
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 mt-5 gap-2">
             {features.map((feature, index) => (
               <div
                 key={index}
                 className="flex justify-center items-center cursor-pointer"
               >
-                <img src={feature} alt="feature" className="max-w-full h-auto" />
+                <img
+                  src={feature}
+                  alt="feature"
+                  className="max-w-full h-auto"
+                />
               </div>
             ))}
           </div>
         </div>
-        <div className="bg-gray-200 w-full h-5"></div>
       </div>
     </div>
   );
